ece709c21c78368c464a1200f7d9e24f606535b5
figo5530 <figooooo0221@gmail.com>
2021-01-19 00:16:20 -0500

    commit tree 98952b45afcc2cdcb1e5febfdbc31c2e175bbdfa

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        arr.select {|arr_word| @word.split("").sort == arr_word.split("").sort}
+        # new_arr = []
+        # arr.each {|arrword| new_arr << arrword if @word.split("").sort == arrword.split("").sort}
+        # new_arr
+    end
+end
